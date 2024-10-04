0245a379472c490a6632952ed9055e196b56e884
Marinus Bijl <9025665@student.zadkine.nl>
2024-10-04 13:12:07 +0200

    Opdracht 6

--- opdracht-4/foo.js
+++ opdracht-4/foo.js
@@ -1,5 +1,5 @@
 function kleur_aanpassen() {
     let boo = document.getElementById("boo");
     boo.style.color = 'blue';
-    boo.textContent = "Inhoud aangepast!"
+    boo.textContent = "Inhoud aangepast!";
 }
---
+++ opdracht-6/foo.js
@@ -0,0 +1,17 @@
+function calculate(num1, num2) {
+    console.log(num1 + num2);
+}
+
+calculate(1, 12);
+
+function multiplyByFive(num) {
+    console.log(num * 5);
+}
+
+multiplyByFive(7);
+
+function calculateMinutes(secs) {
+    console.log(secs / 60);
+}
+
+calculateMinutes(3600)
