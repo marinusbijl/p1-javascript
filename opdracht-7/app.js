173009feb96225009a5d860f87fa8af8c0060d60
Marinus Bijl <9025665@student.zadkine.nl>
2024-10-16 14:41:33 +0200

    Opdracht 7 prompt

--- opdracht-7/app.js
+++ opdracht-7/app.js
@@ -15,3 +15,17 @@ function submit() {
         validatieElement.style.color = "red";
     }
 }
+
+let productPrompt = prompt("Hoeveel kost het product dat je wilt kopen?", "1");
+
+if (productPrompt != null) {
+    product = Number(productPrompt)
+
+    if (budget >= products) {
+        validatieElement.textContent = "U heeft genoeg geld!";
+        validatieElement.style.color = "green";
+    } else {
+        validatieElement.textContent = "Helaas, te weinig geld";
+        validatieElement.style.color = "red";
+    }
+} 
